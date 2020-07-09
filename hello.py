53c93d1ff6e1919efe56d6e1649d61e9417c9b8c
KatsuyaEndoh <k.endo@s-arcana.co.jp>
2020-07-09 20:16:14 +0900

    [add] alice

--- hello.py
+++ hello.py
@@ -1,13 +1,28 @@
 from flask import Flask
-
+from wordcloud import WordCloud
+from os import path
+import os
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    print('hello')
-    return 'Hello World!'
+def hello():
+    return 'Hello!'
+
+
+@app.route('/wordcloud')
+def wordcloud():
+    d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+    text = open(path.join(d, 'sample.txt')).read()
+
+    alice_mask = np.array(Image.open(path.join(d, 'alice_mask.png')))
+
+    wordcloud = WordCloud(background_color='white', max_words=2000, mask=alice_mask, contour_width=3,
+                          contour_color='steelblue').generate(text)
+    return wordcloud.to_svg()
 
 
 if __name__ == '__main__':
